560351f5f8284e3727015893480d7fc6d3c692ca
Christian Mueller <cmuellerrr@gmail.com>
2013-04-21 23:58:40 -0400

    fixed some bugs

--- gnarChart.ino
+++ gnarChart.ino
@@ -6,19 +6,16 @@
 #include "testData.h"
 
 //The circumference of a servo spindle
-#define CIR 3.14
-#define HEIGHT 12
-
-//The time it takes for each servo to fully rotate once.
-//The servos rotate at different speeds, which is probably an
-//issue with how I modified them, but whatever.
-const int rotationSpeed[] = {1700, 1400, 1700, 1600, 1700, 1500, 1500, 1500};
+#define CIR .785
+#define HEIGHT 11
 
 //I/O pins
-const int spotPin = 3;
-switchTracker spotTrack;
-const int metricPin = 4;
+const int metricPin = 3;
 switchTracker metricTrack;
+const int spotPin = 4;
+switchTracker spotTrack;
+const int resetPin = 5;
+switchTracker resetTrack;
 
 //General constants for spots and metrics
 const String spotNames[] = {"Mavericks", "El Porto", "Lower Trestles"};
@@ -32,6 +29,7 @@ void setup() {
   
   initSwitchTrack(spotTrack, spotPin);
   initSwitchTrack(metricTrack, metricPin);
+  initSwitchTrack(resetTrack, resetPin);
   
   setupSpots();
   
@@ -49,19 +47,13 @@ void setupSpots() {
   }
   /*
   for (int i = 0; i < NUM_COLUMNS; i++) {
-     spots[0].values[SWELL][i] = 3;
-     spots[0].values[TIDE][i] = 6;
-     spots[0].values[WIND][i] = 9; 
+    Serial.println(spots[0].values[0][i]);
   }
   for (int i = 0; i < NUM_COLUMNS; i++) {
-     spots[1].values[SWELL][i] = 9;
-     spots[1].values[TIDE][i] = 3;
-     spots[1].values[WIND][i] = 6; 
+    Serial.println(spots[0].values[1][i]);
   }
   for (int i = 0; i < NUM_COLUMNS; i++) {
-     spots[2].values[SWELL][i] = 6;
-     spots[2].values[TIDE][i] = 9;
-     spots[2].values[WIND][i] = 3; 
+    Serial.println(spots[0].values[2][i]);
   }*/
 }
 
@@ -77,8 +69,8 @@ void setupSpot(int spotIndex, String spotName) {
   aJsonObject* root = aJson.parse(json);
 
   parseMetric(root, "forecast", spotIndex, SWELL);
-  parseMetric(root, "tide", spotIndex, TIDE);
   parseMetric(root, "wind", spotIndex, WIND);
+  parseMetric(root, "tide", spotIndex, TIDE);
   //aJson.deleteItem(root);
 }
 
@@ -91,15 +83,21 @@ void parseMetric(aJsonObject* root, char* metric, int spotIndex, int metricIndex
   aJsonObject* v = values->child;
   
   int maxValue = 10;
-  if (metric == "wind") maxValue = 20;
+  if (metric == "wind") {
+    maxValue = 20;
+  }
   
   int columnIndex = 0;
   while (v != 0 && columnIndex < NUM_COLUMNS) {
     
     float curValue = (float)v->valueint;
     curValue = (curValue / maxValue) * HEIGHT;
-    if (curValue < 0) curValue = 0;
-    if (curValue > maxValue) curValue = maxValue;
+    if (curValue < 0) {
+      curValue = 0;
+    }
+    if (curValue > maxValue) {
+      curValue = maxValue;
+    }
     spots[spotIndex].values[metricIndex][columnIndex] = curValue;
       
     columnIndex++;
@@ -113,31 +111,40 @@ void parseMetric(aJsonObject* root, char* metric, int spotIndex, int metricIndex
  * shield.
  */
 char* getSpotJSON(String spotName) {
-  if (spotName == "Mavericks") return json_mavericks;
-  else if (spotName == "Lower Trestles") return json_trestles;
-  else if (spotName == "El Porto") return json_porto;
-  else return "";
+  if (spotName == "Mavericks") {
+    return json_mavericks;
+  } else if (spotName == "Lower Trestles") {
+    return json_trestles;
+  } else if (spotName == "El Porto") {
+    return json_porto;
+  } else {
+    return "";
+  }
 }
 
 /* Main routine (called repeated by from the Arduino framework) */
 void loop() {
   //light LEDs
   
-  if (switchValidPress(spotTrack) && switchValidPress(metricTrack)) {
+  if (switchValidPress(resetTrack)) {
     transition(-1, -1); 
   }
   
   if (switchValidPress(spotTrack)) {
     //change spotIndex
     int newSpot = curSpot + 1;
-    if (newSpot >= NUM_SPOTS) newSpot = 0; 
+    if (newSpot >= NUM_SPOTS) {
+      newSpot = 0; 
+    }
     transition(newSpot, curMetric);
   }
   
   if (switchValidPress(metricTrack)) {
     //set new metric
     int newMetric = curMetric + 1;
-    if (newMetric > TIDE) newMetric = SWELL;
+    if (newMetric > TIDE) {
+      newMetric = SWELL;
+    }
     transition(curSpot, newMetric);
   }
   
@@ -152,28 +159,47 @@ void loop() {
  * Determine the time based off of the predetermined rotation
  * speed and the size of the spindle in relation to the height delta.
  */
-void transition(int newSpot, int newMetric) {
+void transition(int newSpot, int newMetric) {  
   for (int i = 0; i < NUM_COLUMNS; i++) {
     int direc = ROTATE_RIGHT;
     
     //TODO this could get sped up
-    float curValue;
-    if (curSpot < 0 || curMetric < 0) curValue = 0;
-    else curValue = spots[curSpot].values[curMetric][i];
+    float curValue = 0;
+    if (curSpot < 0 || curMetric < 0) {
+      curValue = 0;
+    } else {
+      curValue = spots[curSpot].values[curMetric][i];
+    }
     
     //Reset to 0s if both are negative
-    float newValue;
-    if (newSpot < 0 && newMetric < 0) newValue = 0;
-    else newValue = spots[newSpot].values[newMetric][i];
+    float newValue = curValue;
+    if (newSpot < 0 && newMetric < 0) {
+      newValue = 0;
+      newSpot = 0;
+      newMetric = 0;
+    } else {
+      //Serial.println(spots[newSpot].values[newMetric][i]);
+      newValue = spots[newSpot].values[newMetric][i];
+    }
     
     float delta = newValue - curValue;
+    //Serial.println("Current:");
+    //Serial.println(curValue);
+    //Serial.println("New:");
+    //Serial.println(newValue);
+    //Serial.println("Delta:");
+    //Serial.println(delta);
     
     if (delta < 0) {
        direc = ROTATE_LEFT;
        delta *= -1;
     } 
     
-    float transitionTime = (delta / CIR) * rotationSpeed[i];
+    float transitionTime = (delta / CIR) * 1500;
+    //Serial.println("Rotating: ");
+    //Serial.println(direc);
+    //Serial.println("For: ");
+    //Serial.println(transitionTime);
     servoPut(i, direc);
     delay(transitionTime);
     servoOff(i);
